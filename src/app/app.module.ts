6aec70270479d08e59d7734e91500f2e1fb90cef
rafa <gerardo1022@hotmail.com>
2023-07-22 14:00:12 -0500

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HomeComponent } from './site/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SearchDomainComponent } from './shared/search-domain/search-domain.component';
 import { DomainPriceComponent } from './shared/domain-price/domain-price.component';
+import { GuaranteeComponent } from './shared/guarantee/guarantee.component';
+import { OpinionsComponent } from './shared/opinions/opinions.component';
+import { OurClientsComponent } from './shared/our-clients/our-clients.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { DomainPriceComponent } from './shared/domain-price/domain-price.compone
     HomeComponent,
     HeaderComponent,
     SearchDomainComponent,
-    DomainPriceComponent
+    DomainPriceComponent,
+    GuaranteeComponent,
+    OpinionsComponent,
+    OurClientsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/opinions/opinions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-opinions',
+  templateUrl: './opinions.component.html',
+  styleUrls: ['./opinions.component.css']
+})
+export class OpinionsComponent {
+
+}
